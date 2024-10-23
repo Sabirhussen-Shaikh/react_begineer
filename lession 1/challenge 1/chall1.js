ac5ccb7cd08649731d7e9b4531640bc2d190505c
Sabirhussen shaikh <shaikhsabirhussen@gmail.com>
2024-10-23 22:52:32 +0530

    completing challenge no 1

---
+++ lession 1/challenge 1/chall1.js
@@ -0,0 +1,12 @@
+const page = (
+    <nav>
+        <h1>website</h1>
+        <ul>
+            <li>pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(page , document.getElementById('cont'));
