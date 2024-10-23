663d5dc92f706d2f0d1b291741b1bd82f93beb6a
Sabirhussen shaikh <shaikhsabirhussen@gmail.com>
2024-10-23 20:09:11 +0530

    rendering html using react

---
+++ intro.js
@@ -0,0 +1,6 @@
+// ReactDOM.render(<h1>Sabirhussen Shaikh</h1> , document.getElementById('root'));
+
+ReactDOM.render(<ul>
+    <li>dsa</li>
+    <li>development</li>
+</ul> , document.getElementById('root'));
