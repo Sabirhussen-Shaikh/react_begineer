3396e0fe5332b4a174c08b2173514f69b061cf09
Sabirhussen shaikh <shaikhsabirhussen@gmail.com>
2024-10-23 20:23:59 +0530

    creating and displaying custom made html using react

--- intro.js
+++ intro.js
@@ -4,3 +4,13 @@ ReactDOM.render(<ul>
     <li>dsa</li>
     <li>development</li>
 </ul> , document.getElementById('root'));
+
+function MainContent() {
+    return (
+        <h1> I am learning react !</h1>
+    )
+}
+
+ReactDOM.render(<div>
+    <MainContent/>
+</div> , document.getElementById('root2'));
