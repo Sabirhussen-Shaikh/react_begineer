c27c56768494a2e0e4f3b46625d819363f1679da
Sabirhussen shaikh <shaikhsabirhussen@gmail.com>
2024-10-29 14:42:20 +0530

    project 1 using jsx syntax

--- projectStatic/src/main.jsx
+++ projectStatic/src/main.jsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// method 1 : 
+// createRoot(document.getElementById('root')).render(<App/>)
+
+// method 2: 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
